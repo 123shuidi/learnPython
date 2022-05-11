ff40fbc9aa381357999b064b35582c93f674caf7
水滴WaterDrop <69067308+123shuidi@users.noreply.github.com>
2022-05-11 15:36:10 +0800

    Add files via upload

---
+++ learn_python.py
@@ -0,0 +1,34 @@
+#int            整数
+#float          浮点数(小数)
+#str            字符串
+#list           列表
+#dict           字典
+#bool           布尔
+
+
+#使用方法
+#变量名 = 值
+#int(变量名)
+
+
+#数字
+# a = 100
+# b = 100.1
+# print (int(b))
+
+# a = 100
+# b = 100.1
+# print (float(a))
+# t = int(a) + float(b)
+# t = int(a) + int(b)
+# t = float(a) + float(b)
+# print(t)
+
+
+#字符串
+# a = 100
+# b = 200
+# c = "三联加评论"
+# t = str(a) + str(b)
+# t = str(a) + str(c)
+# print(t)
